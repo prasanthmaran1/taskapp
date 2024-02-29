ec2f4eb19deff8081916c612cdcb9547f83dc0de
prasanth <prasanthm@cenigma.net>
2024-02-29 12:17:31 +0530

    test hooks

--- server/util/order.js
+++ server/util/order.js
@@ -1,6 +1,6 @@
 // this is simple funtion for unit test case
 export const calculateExportCost = (unitsNeeded) => {
-  let exportCostPerBlock = 100;
+  let exportCostPerBlock = 5000;
   const blocks = Math.ceil(unitsNeeded / 10);
   return blocks * exportCostPerBlock;
 };
