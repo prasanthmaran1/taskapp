d0aaa4e4e84fd385f6a1d3f7c927190de49c292b
prasanth <prasanthm@cenigma.net>
2024-02-29 11:59:55 +0530

    Test pre-commit hook

--- server/util/order.js
+++ server/util/order.js
@@ -1,4 +1,4 @@
-// this is simple funtion for unit test cases
+// this is simple funtion for unit test case
 export const calculateExportCost = (unitsNeeded) => {
   let exportCostPerBlock = 5000;
   const blocks = Math.ceil(unitsNeeded / 10);
